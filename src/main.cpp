6b7671f9d83f5d04e64f00db00f7438800efd645
husain3 <arshad.husain1@gmail.com>
2022-12-28 15:55:46 -0700

    Add continuous bluetooth scan

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
-#include <Arduino.h>
+#include "NimBLEDevice.h"
+
+NimBLEScan* pBLEScan;
+
+class MyAdvertisedDeviceCallbacks: public NimBLEAdvertisedDeviceCallbacks {
+    void onResult(NimBLEAdvertisedDevice* advertisedDevice) {
+      Serial.printf("Advertised Device: %s \n", advertisedDevice->toString().c_str());
+    }
+};
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Scanning...");
+
+  NimBLEDevice::setScanFilterMode(CONFIG_BTDM_SCAN_DUPL_TYPE_DEVICE);
+
+  NimBLEDevice::init("");
+
+  pBLEScan = NimBLEDevice::getScan(); //create new scan
+  // Set the callback for when devices are discovered, no duplicates.
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks(), false);
+  pBLEScan->setActiveScan(true); // Set active scanning, this will get more data from the advertiser.
+  pBLEScan->setInterval(97); // How often the scan occurs / switches channels; in milliseconds,
+  pBLEScan->setWindow(37);  // How long to scan during the interval; in milliseconds.
+  pBLEScan->setMaxResults(0); // do not store the scan results, use callback only.
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // If an error occurs that stops the scan, it will be restarted here.
+  if(pBLEScan->isScanning() == false) {
+      // Start scan with: duration = 0 seconds(forever), no scan end callback, not a continuation of a previous scan.
+      pBLEScan->start(0, nullptr, false);
+  }
+
+  delay(2000);
 }
